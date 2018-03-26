78da97069aa32e5f66109c4ed3edc54b5c4becfa
Peter Horn <peter.horn@tickettoaster.de>
2018-03-26 17:32:15 +0200

    crudlle apis

---
+++ lib/cruddler/api_controller.rb
@@ -0,0 +1,185 @@
+#--
+# Copyright (c) 2010-2014 Peter Horn metaminded UG
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+
+module Cruddler::ApiController
+
+  def api_cruddler(methods,
+      klass:              nil,
+      parameter_name:     nil,
+      nested:             nil,
+      nested_as:          nil,
+      resources_name:     nil,
+      resource_name:      nil,
+      name:               nil,
+      edit_attributes:    nil,
+      show_attributes:    nil,
+      index_attributes:   nil,
+      order_by:           'id',
+      order:              'asc',
+      limit:              nil,
+      filter:             nil,
+      &params_block)
+    # get the class that's to be used if it can't be guessed from the controller name
+    klass ||= self.to_s.split("::").last.split("Controller").first.singularize.constantize
+    klass_name = klass.to_s.tableize
+    parameter_name  ||=  klass.to_s.tableize.singularize.gsub('/', '_')
+    nam = "@" + parameter_name
+
+    # if the resource is nested, get the wrapping resources
+    # :nested => :masterclass
+    # :nested => [:supermasterclassname, :masterclassname]
+    # :nested => {:supermasterclassname => SuperMasterClassName, :masterclassname => MasterClassName}
+    nested = nested.presence
+    nested = [nested] if nested.is_a?(String) || nested.is_a?(Symbol)
+    nested = case nested
+    when nil then []
+    when Array then Hash[nested.map{|n| [n.to_s, n.to_s.classify.constantize]}]
+    when Hash then nested
+    else raise "expected :nested Option to get either a list of model-names or a hash name => Class"
+    end
+    before_action :cruddler_get_nested if nested
+    nested_as ||= nested.to_a.last.try(:first)
+
+    # which CRUD methods are to be created?
+    methods = case methods
+    when :all then [:index, :show, :update, :create, :destroy]
+    when :read then [:index, :show]
+    when :none then []
+    else [*methods].flatten
+    end
+
+    resources_name  ||= self.to_s.split("::").last[0..(-11)].tableize
+    resource_name   ||= resources_name.singularize
+    path_components ||= self.to_s.split("::").map(&:underscore)[0..-2]
+
+    # This module will be included later on
+    mod = Module.new
+
+    if block_given?
+      raise "Don't give :edit_attributes option if block is given" if edit_attributes
+      mod.send :define_method, "#{parameter_name}_params" do
+        self.instance_eval &params_block
+      end
+      # private "#{parameter_name}_params"
+    elsif edit_attributes == :all
+      mod.send :define_method, "#{parameter_name}_params" do
+        params.required(parameter_name.to_sym).permit!
+      end
+    elsif edit_attributes.is_a? Proc
+      mod.send :define_method, "#{parameter_name}_params" do
+        pp = self.instance_exec(&edit_attributes)
+        puts pp
+        params.required(parameter_name.to_sym).permit(pp)
+      end
+    elsif edit_attributes
+      mod.send :define_method, "#{parameter_name}_params" do
+        if(klass.respond_to?(:translated_attrs))
+          edit_attributes = Array(edit_attributes).flatten
+          translatable_attrs = klass.translated_attrs.select{|a| edit_attributes.include?(a)}
+          edit_attributes += klass.translation_names_for(translatable_attrs)
+        end
+        params.required(parameter_name.to_sym).permit(edit_attributes)
+      end
+    else
+      if klass.respond_to? :permitted_attributes
+        mod.send :define_method, "#{parameter_name}_params" do
+          edit_attributes = klass.permitted_attributes
+          params.required(parameter_name.to_sym).permit(klass.permitted_attributes)
+        end
+      end
+    end
+
+    mod.send :define_method, :cruddler_params do
+      if self.respond_to? "#{parameter_name}_params"
+        self.send "#{parameter_name}_params"
+      else
+        raise "Either give a block to cruddler, the :edit_attributes option, add `permitted_attributes` in the model, or implement method `#{parameter_name}_params`."
+      end
+    end
+    # private :cruddler_params
+
+    mod.send :define_method, :cruddler do
+      @_cruddler ||= OpenStruct.new(
+          model_name:         nam,
+          klass:              klass,
+          klass_name:         klass_name,
+          nested:             nested,
+          nested_as:          nested_as,
+          parameter_name:     parameter_name,
+          resource_name:      resource_name,
+          resources_name:     resources_name,
+          name:               name,
+          edit_attributes:    edit_attributes,
+          index_attributes:   index_attributes || [:id],
+          show_attributes:    show_attributes || edit_attributes || index_attributes || [:id],
+          order_by:           order_by,
+          order:              order,
+          limit:              limit,
+          filter:             nil,
+        )
+    end
+
+    mod.send :define_method, :cruddler_find_on do
+      if !nested.present? then klass
+      else
+        (cruddler_get_nested.last.send(klass_name.pluralize) rescue klass)
+      end
+    end
+
+    mod.send :define_method, :resource_name do cruddler.resource_name end
+    mod.send :define_method, :resources_name do cruddler.resources_name end
+    mod.send :define_method, :cruddler_get_nested do
+      nested.map do |nam, nklaz|
+        return nil unless params["#{nam}_id"]
+        instance_variable_get("@#{nam}") ||
+        instance_variable_set("@#{nam}", nklaz.find(params["#{nam}_id"]))
+      end
+    end
+
+    # helper
+    mod.send :define_method, :current_object do
+      instance_variable_get(nam)
+    end
+    mod.send :alias_method, :cruddler_current_object, :current_object
+
+    mod.send :define_method, :current_path_components do |*args|
+      [path_components, cruddler_get_nested, args].flatten.compact
+    end
+
+    self.send :include, mod
+
+    include Cruddler::ApiCrudActions::ApiCruddlerData
+    # inlude the desired CRUD actions. There's always just one method per module
+    %w{index show edit update new create destroy}.each do |method|
+      next unless methods.member? method.to_sym
+      include "Cruddler::ApiCrudActions::#{method.capitalize}".constantize
+    end
+
+    helper_method :resource_name, :resources_name,
+      :current_index_path, :current_show_path, :current_new_path,
+      :current_edit_path, :cruddler,
+      :locale_key, :name_for, :current_path_components, :current_name
+  end
+end
+
+ActionController::Base.send :extend, Cruddler::ApiController
---
+++ lib/cruddler/api_crud_actions.rb
@@ -0,0 +1,196 @@
+#--
+# Copyright (c) 2010-2014 Peter Horn metaminded UG
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+
+module Cruddler::ApiCrudActions
+
+  module ApiCruddlerData
+    def cruddler_to_hash(record, attributes)
+      return {} unless record
+      if record.respond_to?(:map)
+        return record.map{|r| cruddler_to_hash(r, attributes)}
+      end
+      res = {}
+      [*attributes].each do |attribute|
+        case attribute
+        when Symbol then res[attribute.to_sym] = record.send(attribute)
+        when String
+          chain = attribute.split('.')
+          if chain.length == 1
+            res[attribute.to_sym] = record.send(attribute)
+          else
+            h = chain[0..-2].inject(res) do |a,e|
+              a[e.to_sym] ||= {}
+              a[e.to_sym]
+            end
+            h[chain.last.to_sym] = chain.inject(record) {|a,e| a.send(e)}
+          end
+        when Hash
+          attribute.each do |k,v|
+            res[k.to_sym] = if v.is_a?(Proc)
+              if v.arity == 0
+                v.()
+              else
+                v.(record)
+              end
+            else
+              nr = if !record.respond_to?(k) && k.to_s.end_with?('_attributes')
+                Hash[
+                  record.send(k.to_s.split('_attributes').first).map do |assoc|
+                    [assoc.id, cruddler_to_hash(assoc, v)]
+                  end
+                ]
+              else
+                record.send(k)
+                cruddler_to_hash(nr, v)
+              end
+            end
+          end
+        else raise "What is #{attribute} (#{attribute.clas})?"
+        end
+      end
+      res
+    end
+
+    def cruddler_render_api_response(data, status=200)
+      respond_to do |format|
+        format.json { render json: data, status: status }
+        format.xml  { render xml: data, status: status }
+        format.html { render html: '<h1>Unsupported Format</h1><p>only <tt>application/json</tt> or <tt>application/xml</tt>.</p>'.html_safe, status: 406 }
+      end
+    end
+
+    def cruddler_render_api_error(data, status)
+      cruddler_render_api_response(data, status)
+    end
+  end
+
+  module Index
+    def cruddler_data_for_index
+      connection = ActiveRecord::Base.connection
+      order_by = params[:order_by] || cruddler.order_by
+      order = params[:order] || cruddler.ordder || 'asc'
+      limit = params[:limit] || cruddler.limit
+      filter = params[:filter].try(:permit!).try(:to_hash) || cruddler.filter || {}
+      @data = cruddler_find_on
+      filter.symbolize_keys.each do |k, vv|
+        k = connection.quote_column_name(k[/[\w\.]*/])
+        if vv.is_a?(Hash)
+          vv.each do |op, v|
+            case op
+            when *%w{= < <= > >= <>} then @data = @data.where("#{k} #{op} ?", v)
+            when *%w{like ilike} then @data = @data.where("#{k} #{op} ?", "%#{v}%")
+            else raise "illegal operand #{op}"
+            end
+          end
+        else
+          @data = @data.where(k => vv.to_s)
+        end
+      end
+      @data = @data.order(order_by => order)
+      @data = @data.limit(limit) if limit
+      # @data = @data.map{|record| cruddler_to_hash(record, cruddler.index_params)}
+      @data
+    end
+
+    def index
+      if cruddler.authorize
+        authorize! :read, cruddler.klass.new
+      end
+      data = cruddler_to_hash cruddler_data_for_index, cruddler.index_attributes
+      cruddler_render_api_response(data)
+    end
+  end
+
+  module Show
+    def show
+      # return render('/cruddler/describe', layout: false) if params[:id] == 'describe'
+      m = cruddler_find_on.find(params[:id])
+      authorize!(:read, m) if cruddler.authorize
+      data = cruddler_to_hash m, cruddler.show_attributes
+      cruddler_render_api_response(data)
+    end
+  end
+
+  module Update
+    def update
+      t = cruddler_find_on.find(params[:id])
+      if cruddler.authorize && !can?(:update, t)
+        return cruddler_render_api_error('not permitted', 401)
+      end
+      success = nil
+      begin
+        success = t.update_attributes(cruddler_params)
+      rescue Exception => e
+        return cruddler_render_api_error(e.message, 500)
+      end
+      if success
+        data = cruddler_to_hash t, cruddler.show_attributes
+        cruddler_render_api_response(data)
+      else
+        data = t.errors.full_messages
+        cruddler_render_api_error(data, 422)
+      end
+    end
+  end
+
+  module Create
+    def create
+      t = cruddler.klass.new(cruddler_params)
+      instance_variable_set(cruddler.model_name, t)
+      if cruddler.authorize && !can?(:create, t)
+        return cruddler_render_api_error('not permitted', 401)
+      end
+      cruddler.nested.to_a.last.try do |name, nklaz|
+        t.send("#{cruddler.nested_as}=", instance_variable_get("@#{name}"))
+      end
+      success = nil
+      begin
+        success = t.save
+      rescue Exception => e
+        return cruddler_render_api_error(e.message, 500)
+      end
+      if success
+        data = cruddler_to_hash t, cruddler.show_attributes
+        cruddler_render_api_response(data)
+      else
+        data = t.errors.full_messages
+        cruddler_render_api_error(data, 422)
+      end
+    end
+  end
+
+  module Destroy
+    def destroy
+      s = cruddler.klass.find(params[:id])
+      authorize!(:destroy, s) if cruddler.authorize
+      begin
+        s.destroy
+      rescue Exception => e
+        return cruddler_render_api_error(e.message, 500)
+      end
+      return cruddler_render_api_response('deleted')
+    end
+  end
+
+
+end
